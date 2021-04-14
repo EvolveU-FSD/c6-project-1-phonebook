d7b3b3d08bc645966acc0efdc8129bbd139db50d
Tony Enerson <tony@enerson.ca>
2021-04-14 12:08:05 -0600

    findAll, searchByName,  delete

--- model/contactsInMongo.js
+++ model/contactsInMongo.js
@@ -1,4 +1,9 @@
+const { ObjectId } = require('mongodb');
 const db = require("./db")
+
+async function getContactsCollection() {
+    return db.getCollection("contacts")
+}
 
 async function createContact(firstName, lastName, phoneNumber) {
     let contactToCreate = {
@@ -6,18 +11,33 @@ async function createContact(firstName, lastName, phoneNumber) {
         lastName,
         phoneNumber
     }
-    let contactsCollection = await db.getCollection("contacts")
+    let contactsCollection = await getContactsCollection()
     let insertResult = await contactsCollection.insertOne(contactToCreate)
     return insertResult.ops[0]._id
 }
 
 async function listContacts() {
-    let contactsCollection = await db.getCollection("contacts")
+    let contactsCollection = await getContactsCollection()
     let cursor = contactsCollection.find({})
     return cursor.toArray()
 }
 
+async function searchByNameFragment(nameFragment) {
+    let matchNameFragment = new RegExp(`.*${nameFragment}.*`, 'i')
+    const collection = await getContactsCollection()
+    let cursor = collection.find({ "firstName": matchNameFragment })
+    const response = await cursor.toArray()
+    return response
+} 
+  
+async function deleteContact(contactId) {
+    const collection = await getContactsCollection()
+    return collection.deleteOne({_id: ObjectId(contactId)})
+}
+
 module.exports = {
     createContact,
-    listContacts
+    listContacts,
+    searchByNameFragment,
+    deleteContact
 }
--- server.js
+++ server.js
@@ -18,6 +18,25 @@ app.post('/api/contact/create', async (req, res) => {
     res.send("Created contact with id " + contactId)
 })
 
+app.get('/api/contact/list', async (req, res) => { 
+  let contactList = await contacts.listContacts() 
+  res.send(contactList) 
+}) 
+
+app.get('/api/contact/searchByNameFragment', async (req, res) => {
+  console.log("Search by name fragment")
+  const nameFragment = req.query.nameFragment
+  console.log(nameFragment)
+  const result = await contacts.searchByNameFragment(nameFragment)  
+  res.send(result)
+}) 
+
+app.delete('/api/contact/:contactId', async (req, res) => { 
+  let contactId = req.params.contactId
+  let deleteResult = await contacts.deleteContact(contactId)
+  res.send("Record deleted: " + deleteResult)
+}) 
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
